ae137418501fab347787af91b74ea69f0b8727cd
Alberto Biancardi <albertobiancardi@gmail.com>
2026-06-21 23:26:26 -0500

    section 65

--- webapp/src/components/Providers.tsx
+++ webapp/src/components/Providers.tsx
@@ -1,11 +1,13 @@
 'use client';
 import { HeroUIProvider } from "@heroui/react";
+import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
+  const router = useRouter();
   // 2. Wrap HeroUIProvider at the root of your app
   return (
-    <HeroUIProvider className="h-full flex flex-col">
+    <HeroUIProvider navigate={router.push} className="h-full flex flex-col">
       {children}
     </HeroUIProvider>
   );
