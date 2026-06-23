609008b560dd16baed0b94924d1f74bbec00c02a
Alberto Biancardi <albertobiancardi@gmail.com>
2026-06-22 22:47:59 -0500

    section 7 67 refactor: update QuestionsPage to use QuestionCard component for rendering questions

--- webapp/src/app/questions/page.tsx
+++ webapp/src/app/questions/page.tsx
@@ -1,14 +1,15 @@
 import { getQuestions } from "@/lib/actions/question-actions"
+import QuestionCard from "./QuestionCard";
 
 export default async function QuestionsPage() {
     const questions = await getQuestions();
   return (
-    <div>
-      <ul>
+    <>
         {questions.map(q => (
-            <li key={q.id}>{q.title}</li>
+            <div key={q.id} className="py-4 not-last:border-b w-full flex">
+                <QuestionCard key={q.id} question={q}/>
+            </div>
         ))}
-      </ul>
-    </div>
+    </>
   )
 }
